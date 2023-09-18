62fdccf31f579ae7c70142c26243c4dc240a51d4
hannahrajarao <hannahrajarao@gmail.com>
2023-09-17 21:52:31 -0400

    Add keys to li in nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,8 +2,8 @@ export default function Nav({ links }) {
     return (
       <nav>
         <ul>
-            {links.map((link) => (
-              <li>{link}</li>
+            {links.map((link, i) => (
+              <li key={i}>{link}</li>
             ))}
         </ul>
       </nav>
