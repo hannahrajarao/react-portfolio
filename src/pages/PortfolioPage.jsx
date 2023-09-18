9abb4a370c3627f009006d801d25fbb224cb294d
hannahrajarao <hannahrajarao@gmail.com>
2023-09-17 21:54:54 -0400

    Populate project portfolio

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,5 +1,41 @@
-import Project from '../components/Project'
+import Project from '../components/Project';
 
 export default function PorfolioPage() {
-    return <Project name='name' link='link' img='img'/>
+    const projectInfo = [
+        {
+            "name": "Shouter",
+            "link": "https://shouterapp-a720f8ccbc1f.herokuapp.com",
+            "imgSrc": "shouter.png"
+        },
+        {
+            "name": "Monochrome Minimalist Minesweeper",
+            "link": "https://hannahrajarao.github.io/monochrome-minimalist-minesweeper",
+            "imgSrc": "monochrome-minimalist-minesweeper.png"
+        },
+        {
+            "name": "Color Guessing Game",
+            "link": "https://hannahrajarao.github.io/color-guessing-game/",
+            "imgSrc": "color-guessing-game.png"
+        },
+        {
+            "name": "Mouse Following Gradients",
+            "link": "https://hannahrajarao.github.io/mouse-following-gradients/flashlight.html",
+            "imgSrc": "mouse-tracker.png"
+        },
+
+    ];
+    const projectList = projectInfo.map(project => 
+        <Project 
+            name={project.name} 
+            link={project.link} 
+            imgSrc={`assets/images/projects/${project.imgSrc}`} 
+            imgDesc={`Screenshot image of ${project.name}`}
+        />
+    );
+    return (<>
+        <h2>Portfolio</h2>
+        <div className='projects'>
+            {projectList}
+        </div>
+    </>);
 }
