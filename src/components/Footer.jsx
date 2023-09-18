626bf6c02979eef2dccbd4cae798fecc10798446
hannahrajarao <hannahrajarao@gmail.com>
2023-09-17 20:49:14 -0400

    Remove text from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 export default function Footer() {
     return (<footer>
         <section id="contact">
-            <h2>Contact</h2>
             <div>
                 <a href="mailto:hannahrajarao@gmail.com" target="_blank"><img src="assets/images/contact-email.png" alt="Envelope icon to click for email" /></a>
                 <a href="https://github.com/hannahrajarao" target="_blank"><img src="assets/images/contact-github.png" alt="GitHub icon to access profile" /></a>
