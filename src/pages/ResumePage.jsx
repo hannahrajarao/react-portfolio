14d42b32bb0a498d998e8104021a8fadca192603
hannahrajarao <hannahrajarao@gmail.com>
2023-09-17 20:43:54 -0400

    Add resume

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,3 +1,16 @@
 export default function ResumePage() {
-    
+    return (<>
+    <h2>Resume</h2>
+    <p>View <a href="https://drive.google.com/file/d/1gjXRuRwRn0h0rooxuQqPrKq69Q1f3wbQ/view">here</a></p>
+    <h3>Education</h3>
+    <h4>Full Stack Web Development Certificate Bootcamp</h4>
+    <h4>B.E. in Mechanical Engineering, Minor in Computer Science</h4>
+    <h3>Experience</h3>
+    <h4>Test Engineer</h4>
+    <ul>
+    <li>Monitor test performance from production line through analysis of data</li>
+    <li>Based on data analysis, identify hardware problems, validate and document all errors discovered, and
+    debug/implement solutions</li>
+    </ul>
+    </>);
 }
