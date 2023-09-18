f935b27dfa91284bb6ba7689d5f8527983f7033d
hannahrajarao <hannahrajarao@gmail.com>
2023-09-17 21:52:53 -0400

    Change class to className

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,6 @@
 export default function Project(props) {
     return (
-        <div class="card">
+        <div className="card">
             <h3>{props.name}</h3>
             <a href="https://hannahrajarao.github.io/location-weather/" target="_blank"><img src="assets/images/projects/location-weather.png" alt="Screenshot of location and weather project website" /></a>
         </div>
